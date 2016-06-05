0b4e6833f126befc4c6d860521a2f847e904f1c1
Eric Hocking <xaoseric@users.noreply.github.com>
2016-06-05 13:30:16 -0400

    upload classes

---
+++ src/Additional.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Whoisdoma\DomainParser;
+
+$additional = array(
+        'uk' => array('co.uk', 'me.uk', 'net.uk', 'org.uk', 'sch.uk', 'ac.uk', 'gov.uk', 'nhs.uk', 
+                'police.uk', 'mod.uk'), 'au' => array('csiro.au'), 
+        'ke' => array('co.ke', 'or.ke', 'ne.ke', 'go.ke', 'ac.ke', 'sc.ke', 'me.ke', 'mobi.ke', 
+                'info.ke'), 
+        'tr' => array('com.tr', 'gen.tr', 'org.tr', 'biz.tr', 'info.tr', 'name.tr', 'net.tr', 
+                'web.tr', 'edu.tr'), 
+        'nz' => array('ac.nz', 'co.nz', 'geek.nz', 'gen.nz', 'maori.nz', 'net.nz', 'org.nz', 
+                'school.nz'), 
+        'il' => array('ac.il', 'co.il', 'org.il', 'net.il', 'k12.il', 'gov.il', 'muni.il', 'idf.il'), 
+        'ck' => array('co.ck', 'edu.ck', 'gov.ck', 'net.ck', 'org.ck'), 
+        'fj' => array('ac.fj', 'biz.fj', 'com.fj', 'info.fj', 'mil.fj', 'name.fj', 'net.fj', 
+                'org.fj', 'pro.fj'), 
+        
+        // private domain names
+        'centralnic' => array('ae.org', 'ar.com', 'br.com', 'cn.com', 'de.com', 'eu.com', 'gb.com', 
+                'gb.net', 'gr.com', 'hu.com', 'hu.net', 'jp.net', 'jpn.com', 'kr.com', 'no.com', 
+                'qc.com', 'ru.com', 'sa.com', 'se.com', 'se.net', 'uk.com', 'uk.net', 'us.com', 
+                'us.org', 'uy.com', 'za.com', 'com.de'), 'de.vu' => array('de.vu'), 
+        'co.cc' => array(), 'com.cc' => array('com.cc'), 'org.cc' => array('org.cc'), 
+        'edu.cc' => array('edu.cc'), 'net.cc' => array('net.cc'));
